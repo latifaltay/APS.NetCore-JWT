df85d019c9be9eb961c9b02c986d33d0ca2e1674
latifaltay <latifaltay00@gmail.com>
2025-08-27 04:34:06 +0300

    IAuthenticationService has been developed

---
+++ AuthServerForJWT_Edu.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using AuthServerForJWT_Edu.Core.DTOs;
+using SharedLibrary.Dtos;
+
+namespace AuthServerForJWT_Edu.Core.Services;
+
+public interface IAuthenticationService
+{
+    Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+    Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+    Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+    Response<ClientTokenDto> CreateTokenByClient(ClientLoginDto clientLoginDto);
+}
