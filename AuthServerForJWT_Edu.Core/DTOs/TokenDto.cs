2dfb985dcd45259c0bfe63cd5650ae32bdddf01c
latifaltay <latifaltay00@gmail.com>
2025-08-28 02:12:36 +0300

    Token Service has been developed

--- AuthServerForJWT_Edu.Core/DTOs/TokenDto.cs
+++ AuthServerForJWT_Edu.Core/DTOs/TokenDto.cs
@@ -5,5 +5,5 @@ public class TokenDto
     public string AccessToken { get; set; }
     public DateTime AccessTokenExpiration { get; set; }
     public string RefreshToken { get; set; }
-    public DateTime RefreshTokenExprition { get; set; }
+    public DateTime RefreshTokenExpiration { get; set; }
 }
