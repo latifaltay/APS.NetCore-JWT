e4e97fa6441a28f6b0a6894110eb354eb4162fb7
latifaltay <latifaltay00@gmail.com>
2025-08-27 06:41:10 +0300

    AppDbContext has been created and also entity configuration has been added

---
+++ AuthServerForJWT_Edu.Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using AuthServerForJWT_Edu.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServerForJWT_Edu.Data;
+
+public class AppDbContext : IdentityDbContext<UserApp, IdentityRole, string>
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+    {
+    }
+
+    public DbSet<Product> Produts { get; set; }
+
+    public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+        base.OnModelCreating(builder);
+    }
+}
