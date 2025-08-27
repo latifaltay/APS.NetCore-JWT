358928327c509218ce9d18c8752a41645f8ecada
latifaltay <latifaltay00@gmail.com>
2025-08-27 07:05:42 +0300

    UnitOfWork and Generic repository classes has been developed

--- AuthServerForJWT_Edu.Core/Repositories/IGenericRepository.cs
+++ AuthServerForJWT_Edu.Core/Repositories/IGenericRepository.cs
@@ -6,7 +6,7 @@ public interface IGenericRepository<TEntity> where TEntity : class
 {
     Task<TEntity> GetByIdAsync(int id);
     IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
-    Task<IQueryable<TEntity>> GetAllAsync();
+    Task<IEnumerable<TEntity>> GetAllAsync();
     Task AddAsync(TEntity entity);
     void Remove(TEntity entity);
     TEntity Update(TEntity entity);
---
+++ AuthServerForJWT_Edu.Data/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using AuthServerForJWT_Edu.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServerForJWT_Edu.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DbContext _context;
+
+    public UnitOfWork(AppDbContext appDbContext)
+    {
+        _context = appDbContext;
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public async Task CommmitAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
